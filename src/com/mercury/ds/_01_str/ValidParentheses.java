1c7189c527f1310e30f4df220cfea749a0fcf423
mercury <mercuryTan@163.com>
2021-03-15 17:16:39 +0800

    string: ValidParentheses

---
+++ src/com/mercury/ds/_01_str/ValidParentheses.java
@@ -0,0 +1,81 @@
+package com.mercury.ds._01_str;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for (char currentChar : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                stack.push(currentChar);
+                continue;
+            }
+
+            char topC = stack.peek();
+            if(isClose(topC,currentChar)) {
+                stack.pop();
+            } else {
+                stack.push(currentChar);
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+    public static boolean isClose(char c1, char c2) {
+        if (c1 == '(' && c2 == ')') {
+            return true;
+        }
+        if (c1 == '{' && c2 == '}') {
+            return true;
+        }
+        if (c1 == '[' && c2 == ']') {
+            return true;
+        }
+        return false;
+    }
+
+}
+
+
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：s = "()"
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * 输入：s = "()[]{}"
+ * 输出：true
+ * 示例 3：
+ * <p>
+ * 输入：s = "(]"
+ * 输出：false
+ * 示例 4：
+ * <p>
+ * 输入：s = "([)]"
+ * 输出：false
+ * 示例 5：
+ * <p>
+ * 输入：s = "{[]}"
+ * 输出：true
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= s.length <= 104
+ * s 仅由括号 '()[]{}' 组成
+ **/
